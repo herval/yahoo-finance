b5438143c9111cd4ef5e7afcc95df16f2ecd2b3d
fengb <contact@fengb.info>
2014-08-04 20:54:27 -0500

    Split tests up for easier debugging

--- test/yahoo_finance_test.rb
+++ test/yahoo_finance_test.rb
@@ -3,27 +3,21 @@ require 'test/unit'
 require File.join(File.dirname(__FILE__),'../lib/yahoo_finance')
 
 class TestYahoo_finance_test < Test::Unit::TestCase
-  def test_quotes
+  def test_simple_quotes
     quotes = YahooFinance.quotes(["BVSP", "AAPL"])
     assert_equal(2, quotes.size)
-    
-    assert_nothing_raised do
-      q = YahooFinance.historical_quotes("MSFT", Time::now-(24*60*60*40), Time::now, { :raw => false, :period => :daily })
-      [:trade_date, :open, :high, :low, :close, :volume, :adjusted_close].each do |col|
-        assert q.first.send(col)
-      end
-    end
+  end
 
-    assert_nothing_raised do
-     q = YahooFinance.historical_quotes("MSFT", Time::now-(24*60*60*400), Time::now, { :raw => false, :period => :dividends_only })
-     assert q.first.dividend_pay_date
-     assert q.first.dividend_yield
-    end
-     
-    assert_nothing_raised do
-      YahooFinance.quotes(["AAPL", "MSFT", "BVSP", "JPYUSD" ],
-                          YahooFinance::COLUMNS.take(20).collect { |k, v| v },
-                          { raw: false })
+  def test_custom_columns
+    YahooFinance.quotes(["AAPL", "MSFT", "BVSP", "JPYUSD" ],
+                        YahooFinance::COLUMNS.take(20).collect { |k, v| v },
+                        { raw: false })
+  end
+
+  def test_historical_quotes
+    q = YahooFinance.historical_quotes("MSFT", Time::now-(24*60*60*40), Time::now, { :raw => false, :period => :daily })
+    [:trade_date, :open, :high, :low, :close, :volume, :adjusted_close].each do |col|
+      assert q.first.send(col)
     end
   end
 
@@ -32,6 +26,12 @@ class TestYahoo_finance_test < Test::Unit::TestCase
     assert s.first.date
     assert s.first.before
     assert s.first.after
+  end
+
+  def test_dividends
+    q = YahooFinance.historical_quotes("MSFT", Time::now-(24*60*60*400), Time::now, { :raw => false, :period => :dividends_only })
+    assert q.first.dividend_pay_date
+    assert q.first.dividend_yield
   end
 
   def test_escapes_symbol_for_url
